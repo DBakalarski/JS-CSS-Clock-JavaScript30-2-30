aa21c80739e2bb328b68674b2f8af738c3868197
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-09-20 13:48:09 +0200

    Finish excercise

---
+++ index.js
@@ -0,0 +1,28 @@
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+function setDate() {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const minutes = now.getMinutes();
+	const hours = now.getHours();
+
+	const secondsDegrees = ((seconds / 60) * 360 )+ 90;
+	const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+	const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30 )+ 90;
+
+	if (secondsDegrees> 90 && secondsDegrees <= 444) {
+		secondHand.classList.add("hand-transition");
+	}
+	else {
+		secondHand.classList.remove("hand-transition");
+	}
+
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+	minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000 )
